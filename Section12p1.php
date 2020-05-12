2c7088a1d91b9a59a9a41044df5a6e39d5eef741
Jenniline <jennilineebai@gmail.com>
2020-05-12 09:54:03 +0100

    Second commit

---
+++ Section12p1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+        <p>initializing an array</p>
+    <?php
+    echo"An empty array";
+    $foo = array();
+    //or
+    $foo= [];
+
+    echo"An array can be initialized an preset with values";
+
+    ?>
+</body>
+</html>
