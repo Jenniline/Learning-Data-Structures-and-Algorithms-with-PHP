0c04df804b59cf02a44cac3b473afc30ff9149e5
Jenniline <jennilineebai@gmail.com>
2020-09-17 14:33:17 +0100

    elaborated on the comment message for the string append file

--- string_append.php
+++ string_append.php
@@ -6,7 +6,7 @@ echo $name;
 echo "alskdhf";
 echo "<br>";
 
-//This did not work I still have to learn this Algorithm
+//This did not work I still have to learn this Algorithm.Still have to learn the algorithm to append and empty string 
 echo '';
 echo 2; 
 
