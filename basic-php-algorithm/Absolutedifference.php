d56179d7eac1916d63c33b6babfecdc3eb7a4575
Jenniline <jennilineebai@gmail.com>
2022-08-17 15:01:09 +0100

    Used 2 solutions to get the absolute difference of 2 unidentical numbers

--- basic-php-algorithm/Absolutedifference.php
+++ basic-php-algorithm/Absolutedifference.php
@@ -13,3 +13,23 @@ echo absoluteDifference(53). "\n";
 echo $a =  absoluteDifference(30). "\n";
 echo abs($a) . "\n";
 echo absoluteDifference(51). "\n";
+
+function test($n)
+{
+    $x = 51;
+
+    if ($n > $x) {
+        return ($n > $x) * 3;
+    }
+
+    $n - $x;
+
+}
+
+echo "Test output";
+echo test(53). "\n";
+
+echo $b =  abs(test(30)). "\n";
+
+
+echo test(51). "\n";
