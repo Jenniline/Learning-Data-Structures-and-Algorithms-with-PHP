03ee72c9979934c75b77e4ff24ff1a086498a440
Jenniline <jennilineebai@gmail.com>
2022-08-22 00:46:22 +0100

    Solving PHP interview questions

---
+++ basic-php-algorithm/checktwoIntegers.php
@@ -0,0 +1,11 @@
+<!-- Write a PHP program to check two given integers, 
+and return true if one of them is 30 or if their sum is 30 -->
+
+<?php
+
+function test( int $a,int $b) 
+{
+    return ($a == 30) || ($b == 30) || ($a + $b == 30);
+}
+
+var_dump(test(30.0000, 0));
